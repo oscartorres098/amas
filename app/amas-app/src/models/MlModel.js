18da227c390560d0ba5c3066f06ea7eaffec42dc
oscartorres098 <36300388+oscartorres098@users.noreply.github.com>
2019-09-14 17:07:22 -0500

    modelo esquena

---
+++ app/amas-app/src/models/MlModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const MlModelSchema = new Schema({
+  scaler: {
+    type: String
+  },
+  pre_method: {
+    type: String
+  },
+  train_method: {
+    type: String
+  },
+  rr: {
+    type: Number,
+    required: true
+  },
+  mse: {
+    type: Number,
+  },
+  modelo: {
+    type: Buffer,
+  },
+  fechaSubida: {
+    type: Date,
+    default: Date.now
+  },
+  detail: {
+    type: String
+  },
+  user: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Sample", MlModelSchema);
